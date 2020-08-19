6c7a56d142f5c47fba9b9e903b3dbd5576a01db1
Patrick Cameron <patcameron@gmail.com>
2020-08-18 22:11:55 -0400

    Fix test script for Calculator component

--- src/app/components/calculator/calculator.component.spec.ts
+++ src/app/components/calculator/calculator.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CalculatorComponent } from './calculator.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('CalculatorComponent', () => {
   let component: CalculatorComponent;
@@ -8,6 +9,7 @@ describe('CalculatorComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ ReactiveFormsModule ],
       declarations: [ CalculatorComponent ]
     })
     .compileComponents();
